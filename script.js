94111657ddaafe541975fe7c5e398a9f544d9c70
Gonai <margaret.szymanik@gmail.com>
2024-11-04 11:33:47 +0100

    banner javasctipt swap

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// MENU
 const menu = document.querySelector(".menu");
 const nav = document.querySelector("nav");
 
@@ -5,3 +6,31 @@ menu.addEventListener("click", function () {
     menu.classList.toggle("on");
     nav.classList.toggle("on");
 })
+
+// BANNER
+let activeElement = 0;
+const timeChange = 5000;
+
+const colorImgHtml = document.querySelector('.color');
+const grayImgHtml = document.querySelector('.gray');
+const h1Html = document.querySelector('.info h1');
+const h2Html = document.querySelector('.info h2');
+
+const colorImages = ['img/banner_girl_color.png', 'img/banner_boy_color.png'];
+const grayImages = ['img/banner_girl_bw.png', 'img/banner_boy_bw.png'];
+const h1Info = ['Front-End Developer', 'Portfolio projektów'];
+const h2Info = ['Margaret S.', 'aplikacji webowych'];
+
+function changeElement() {
+ activeElement++;
+ if (activeElement == colorImages.length) {
+  activeElement = 0;
+ }
+ colorImgHtml.src = colorImages[activeElement];
+ grayImgHtml.src = grayImages[activeElement];
+ h1Html.textContent = h1Info[activeElement];
+ h2Html.textContent = h2Info[activeElement];
+
+}
+
+setInterval(changeElement, timeChange)

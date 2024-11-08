693c2995f7de8303f1b95ecd0bc4d09f1e40acb4
Gonai <margaret.szymanik@gmail.com>
2024-11-08 15:04:46 +0100

    update section contact, add mail.php

--- script.js
+++ script.js
@@ -95,3 +95,29 @@ spanHide3.addEventListener("click", function () {
     main.classList.remove("blur");
     header.classList.remove("blur");
 })
+
+// CONTACT MAIL
+
+const msgStatus = document.querySelector(".msg-status");
+
+console.log(document.location.search);
+
+if (document.location.search === '?mail_status=sent') {
+    msgStatus.classList.add("success")
+    msgStatus.textContent = "Wiadomość wysłana!"
+
+    setTimeout(() => {
+        msgStatus.classList.remove("success")
+    }, 3000);
+}
+
+if (document.location.search === '?mail_status=error') {
+    msgStatus.classList.add("error")
+    msgStatus.textContent = "Wystąpił błąd..."
+
+    setTimeout(() => {
+        msgStatus.classList.remove("error")
+    }, 3000);
+}
+
+
